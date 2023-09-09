d0442f209c4b95fcdf73697e58440a588152f433
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2023-09-08 22:41:33 -0500

    Proyecto creado

---
+++ src/main/java/com/cibertec/SpringEjemplo01Application.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEjemplo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEjemplo01Application.class, args);
+	}
+
+}
